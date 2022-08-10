f80873b578ef3d2c8fe67d32f71c27298a9875f0
Vicky <vigran09@icloud.com>
2022-08-10 19:21:52 +0530

    Update index.js
     Updated index.js for Berlin locations

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import {useEffect, useRef} from 'react';
 import {Loader} from '@googlemaps/js-api-loader';
 import React from "react";
-import locations from '../locationsChennai.json';
+import locations from '../locations.json';
 
 const Maps = () => {
 
@@ -10,44 +10,24 @@ const Maps = () => {
   useEffect(() => {
     const loader = new Loader({
       apiKey: 'AIzaSyAqJUf7ZdKXNZO8KG8mC6VHDv-tiHy_QhI',
-      version: 'weekly',
     });
     let map; 
     loader.load().then(() => {
       const google = window.google;
-      map = new google.maps.Map(googlemap.current, {
-        center: {lat: 12.8996, lng: 80.2209},
-        zoom: 13, mapId: '8ee3dc81b152ef98'
-        ,fullscreenControl: false, 
-        mapTypeControl: false, 
-        streetViewControl: false, 
-        zoomControl: false, 
-      }
-      );
+      navigator.geolocation.getCurrentPosition(success);
 
-    for (let i = 0; i < locations.places.length; i++) {
-        const userPlace = locations.places[i];
-        new google.maps.Marker({
-          map: map,
-          position: {
-            lat: userPlace.latitude,
-            lng: userPlace.longitude,
-          },
-          title: userPlace.name,
-        });
-      }
-      const icon = {
-        url: 'https://i.imgur.com/9v6uW8U.png', 
-        scaledSize: new google.maps.Size(40,40), 
-        origin: new google.maps.Point(0,0), 
-        anchor: new google.maps.Point(0, 0) 
-    };
       function success(pos) {
         const crd = pos.coords;
-        console.log('Your current position is:');
-        console.log(`Latitude : ${crd.latitude}`);
-        console.log(`Longitude: ${crd.longitude}`);
-        console.log(`More or less ${crd.accuracy} meters.`);
+
+        map = new google.maps.Map(googlemap.current, {
+          center: {lat: crd.latitude, lng: crd.longitude},
+          zoom: 13, mapId: '8ee3dc81b152ef98'
+          ,fullscreenControl: false, 
+          mapTypeControl: false, 
+          streetViewControl: false, 
+          zoomControl: false, 
+        }
+        );
         const marker = new google.maps.Marker({
             map: map,
             position: {
@@ -60,9 +40,25 @@ const Maps = () => {
           marker.addListener("click", () => {
              console.log("marker");           
           });
+      
+          for (let i = 0; i < locations.places.length; i++) {
+            const userPlace = locations.places[i];
+            new google.maps.Marker({
+              map: map,
+              position: {
+                lat: userPlace.latitude,
+                lng: userPlace.longitude,
+              },
+              title: userPlace.name,
+            });
+          }
       };
-    navigator.geolocation.getCurrentPosition(success);
-    
+      const icon = {
+        url: 'https://i.imgur.com/9v6uW8U.png', 
+        scaledSize: new google.maps.Size(40,40), 
+        origin: new google.maps.Point(0,0), 
+        anchor: new google.maps.Point(0, 0) 
+    };
     });
   });
   return (
