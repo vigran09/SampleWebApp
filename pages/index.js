a3da7fa0c5fc118c283433acd20168a2a04263b2
Vicky <vigran09@icloud.com>
2022-09-19 09:55:26 +0530

    Changes in Index

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,6 @@
 import {useEffect, useRef} from 'react';
 import {Loader} from '@googlemaps/js-api-loader';
 import React from "react";
-import {
-  AmbientLight,
-  DirectionalLight,
-  Matrix4,
-  PerspectiveCamera,
-  Scene,
-  WebGLRenderer,
-} from "three";
-
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import locations from '../locations.json';
  
 const Maps = () => {
@@ -21,109 +11,44 @@ const Maps = () => {
     version: "beta"
   };
   const mapOptions = {
-    "center": { lat: 52.498013281042056,  lng: 13.351595043107444 },
-    "zoom": 17,
+    "zoom": 18,
     "heading": 320,
     "tilt": 47.5,
   }
+
+  let map;
+  var mapzoom = 18;
   useEffect(() => {
     const loader = new Loader(apiOptions);
     loader.load().then(() => {
       const google = window.google;
-      const map = new google.maps.Map(googlemap.current,  {
+      map = new google.maps.Map(googlemap.current,  {
         center: { lat: 52.498013281042056,  lng: 13.351595043107444 },
-        zoom: 18,
-        heading: 320,
-        tilt: 47.5,
+        zoom: 13,
         mapId:"d78e050a3a97c54b",
         mapTypeControl: false,
         streetViewControl: false, 
       }
-      );   
-      // initWebglOverlayView(map);
+      ); 
       AddMarkers(map);
     });
 
-    function initWebglOverlayView(map){
-      let scene, renderer, camera, loader;
-      const webglOverlayView = new google.maps.WebGLOverlayView();
-    
-      webglOverlayView.onAdd = () => {
-        // Set up the scene.
-    
-        scene = new Scene();
-    
-        camera = new PerspectiveCamera();
-    
-        const ambientLight = new AmbientLight(0xffffff, 0.75); // Soft white light.
-        scene.add(ambientLight);
-    
-        const directionalLight = new DirectionalLight(0xffffff, 0.25);
-        directionalLight.position.set(0.5, -1, 0.5);
-        scene.add(directionalLight);
-    
-        // Load the model.
-        loader = new GLTFLoader();
-        const source =
-          "https://raw.githubusercontent.com/googlemaps/js-samples/main/assets/pin.gltf";
-        loader.load(source, (gltf) => {
-          gltf.scene.scale.set(7, 7, 7);
-          gltf.scene.rotation.x = Math.PI; // Rotations are in radians.
-          scene.add(gltf.scene);
-        });
-      };
-    
-      webglOverlayView.onContextRestored = ({ gl }) => {
-        // Create the js renderer, using the
-        // maps's WebGL rendering context.
-        renderer = new WebGLRenderer({
-          canvas: gl.canvas,
-          context: gl,
-          ...gl.getContextAttributes(),
-        });
-        renderer.autoClear = false;
-    
-        // Wait to move the camera until the 3D model loads.
-        loader.manager.onLoad = () => {
-          renderer.setAnimationLoop(() => {
-            webglOverlayView.requestRedraw();
-            const { tilt, heading, zoom } = mapOptions;
-            map.moveCamera({ tilt, heading, zoom });
-    
-            // Rotate the map 360 degrees.
-            if (mapOptions.tilt < 67.5) {
-              mapOptions.tilt += 0.5;
-            } else if (mapOptions.heading <= 360) {
-              mapOptions.heading += 0.2;
-              mapOptions.zoom -= 0.0005;
-            } else {
-              renderer.setAnimationLoop(null);
-            }
-          });
-        };
-      };
-    
-      webglOverlayView.onDraw = ({ gl, transformer })=> {
-        const latLngAltitudeLiteral= {
-          lat: 52.498013281042056,
-          lng: 13.351595043107444,
-          altitude: 50,
-        };
-    
-        // Update camera matrix to ensure the model is georeferenced correctly on the map.
-        const matrix = transformer.fromLatLngAltitude(latLngAltitudeLiteral);
-        camera.projectionMatrix = new Matrix4().fromArray(matrix);
-    
-        webglOverlayView.requestRedraw();
-        renderer.render(scene, camera);
-    
-        // Sometimes it is necessary to reset the GL state.
-        renderer.resetState();
-      };
-      webglOverlayView.setMap(map);
+    function LocateStation(marker, event){
+      map.setCenter(event.latLng.lat(),event.latLng.lng());
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      const { tilt, heading, zoom } = mapOptions;
+      map.moveCamera({tilt, heading, zoom });
+      requestAnimationFrame(animateCamera);
     }
 
-    function AddMarkers(map){
+    function animateCamera(time) {
+      map.moveCamera({
+        heading: (time / 1000) * 5
+      });
+      requestAnimationFrame(animateCamera);
+    }
+
+    function AddMarkers(){
       for (let i = 0; i < locations.places.length; i++) {
         const userPlace = locations.places[i];
         const marker = new google.maps.Marker({
@@ -137,7 +62,8 @@ const Maps = () => {
         marker.addListener("click", event => {
           console.log(userPlace.name);  
           const location = { lat: event.latLng.lat(), lng: event.latLng.lng() };
-          map.panTo(location);         
+          map.panTo(location); 
+          LocateStation(marker, event);        
        });
       }
       const icon = {
--- pages/three.js
+++ pages/three.js
@@ -36,6 +36,86 @@ const Maps = () => {
       );   
       AddMarkers();
     });
+
+    // function initWebglOverlayView(map){
+    //   let scene, renderer, camera, loader;
+    //   const webglOverlayView = new google.maps.WebGLOverlayView();
+    
+    //   webglOverlayView.onAdd = () => {
+    //     // Set up the scene.
+    
+    //     scene = new Scene();
+    
+    //     camera = new PerspectiveCamera();
+    
+    //     const ambientLight = new AmbientLight(0xffffff, 0.75); // Soft white light.
+    //     scene.add(ambientLight);
+    
+    //     const directionalLight = new DirectionalLight(0xffffff, 0.25);
+    //     directionalLight.position.set(0.5, -1, 0.5);
+    //     scene.add(directionalLight);
+    
+    //     // Load the model.
+    //     loader = new GLTFLoader();
+    //     const source =
+    //       "https://raw.githubusercontent.com/googlemaps/js-samples/main/assets/pin.gltf";
+    //     loader.load(source, (gltf) => {
+    //       gltf.scene.scale.set(7, 7, 7);
+    //       gltf.scene.rotation.x = Math.PI; // Rotations are in radians.
+    //       scene.add(gltf.scene);
+    //     });
+    //   };
+    
+    //   webglOverlayView.onContextRestored = ({ gl }) => {
+    //     // Create the js renderer, using the
+    //     // maps's WebGL rendering context.
+    //     renderer = new WebGLRenderer({
+    //       canvas: gl.canvas,
+    //       context: gl,
+    //       ...gl.getContextAttributes(),
+    //     });
+    //     renderer.autoClear = false;
+    
+    //     // Wait to move the camera until the 3D model loads.
+    //     loader.manager.onLoad = () => {
+    //       renderer.setAnimationLoop(() => {
+    //         webglOverlayView.requestRedraw();
+    //         const { tilt, heading, zoom } = mapOptions;
+    //         map.moveCamera({ tilt, heading, zoom });
+    
+    //         // Rotate the map 360 degrees.
+    //         if (mapOptions.tilt < 67.5) {
+    //           mapOptions.tilt += 0.5;
+    //         } else if (mapOptions.heading <= 360) {
+    //           mapOptions.heading += 0.2;
+    //           mapOptions.zoom -= 0.0005;
+    //         } else {
+    //           renderer.setAnimationLoop(null);
+    //         }
+    //       });
+    //     };
+    //   };
+    
+    //   webglOverlayView.onDraw = ({ gl, transformer })=> {
+    //     const latLngAltitudeLiteral= {
+    //       lat: 52.498013281042056,
+    //       lng: 13.351595043107444,
+    //       altitude: 50,
+    //     };
+    
+    //     // Update camera matrix to ensure the model is georeferenced correctly on the map.
+    //     const matrix = transformer.fromLatLngAltitude(latLngAltitudeLiteral);
+    //     camera.projectionMatrix = new Matrix4().fromArray(matrix);
+    
+    //     webglOverlayView.requestRedraw();
+    //     renderer.render(scene, camera);
+    
+    //     // Sometimes it is necessary to reset the GL state.
+    //     renderer.resetState();
+    //   };
+    //   webglOverlayView.setMap(map);
+    // }
+
     function AddMarkers(){
       for (let i = 0; i < locations.places.length; i++) {
         const userPlace = locations.places[i];
