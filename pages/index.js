6064fde0a21017af189d6019ce6c0bc930b16bee
Vicky <vigran09@icloud.com>
2022-09-19 14:40:23 +0530

    Cleared Unused pages and codes

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,14 @@ const Maps = () => {
   }
 
   let map;
-  var mapzoom = 18;
   useEffect(() => {
     const loader = new Loader(apiOptions);
     loader.load().then(() => {
+      LoadMap();
+      AddMarkers(map);
+    });
+
+    function LoadMap(){
       const google = window.google;
       map = new google.maps.Map(googlemap.current,  {
         center: { lat: 52.498013281042056,  lng: 13.351595043107444 },
@@ -30,8 +34,7 @@ const Maps = () => {
         streetViewControl: false, 
       }
       ); 
-      AddMarkers(map);
-    });
+    }
 
     function LocateStation(marker, event){
       map.setCenter(event.latLng.lat(),event.latLng.lng());
@@ -59,11 +62,44 @@ const Maps = () => {
           },
           title: userPlace.name,
         });
+        const contentString =
+        '<div id="content">' +
+        '<div id="siteNotice">' +
+        "</div>" +
+        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+        '<div id="bodyContent">' +
+        "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+        "sandstone rock formation in the southern part of the " +
+        "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+        "south west of the nearest large town, Alice Springs; 450&#160;km " +
+        "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+        "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+        "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+        "Aboriginal people of the area. It has many springs, waterholes, " +
+        "rock caves and ancient paintings. Uluru is listed as a World " +
+        "Heritage Site.</p>" +
+        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+        "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+        "(last visited June 22, 2009).</p>" +
+        "</div>" +
+        "</div>";
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString,
+        });
         marker.addListener("click", event => {
           console.log(userPlace.name);  
           const location = { lat: event.latLng.lat(), lng: event.latLng.lng() };
           map.panTo(location); 
-          LocateStation(marker, event);        
+          infowindow.open({
+            anchor: marker,
+            map,
+          });
+          infowindow.addListener('closeclick', ()=>{
+            LoadMap();
+            AddMarkers(map);
+            map.setHeading(0);
+          });
+          LocateStation(marker, event);    
        });
       }
       const icon = {
